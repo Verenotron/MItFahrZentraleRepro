dc247abd08041e595db36101a8ea1f246a4f1465
Verena <Verena.kipp@student.hs-rm.de>
2025-03-12 19:11:31 +0100

    FEAT: first commit

---
+++ src/main/java/com/WebProjekt/MItfahrZentrale/MItfahrZentraleApplication.java
@@ -0,0 +1,13 @@
+package com.WebProjekt.MItfahrZentrale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MItfahrZentraleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MItfahrZentraleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WebProjekt/MItfahrZentrale/MItfahrZentraleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebProjekt.MItfahrZentrale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MItfahrZentraleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
