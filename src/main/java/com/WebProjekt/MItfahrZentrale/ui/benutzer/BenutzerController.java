9fc540cdd945fbd7eb279b29342c8f81380654d4
Verena <Verena.kipp@student.hs-rm.de>
2025-03-13 13:28:51 +0100

    FEAT: Values saved in Session

---
+++ src/main/java/com/WebProjekt/MItfahrZentrale/ui/benutzer/BenutzerController.java
@@ -0,0 +1,49 @@
+package com.WebProjekt.MItfahrZentrale.ui.benutzer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+
+
+@Controller
+@SessionAttributes("benutzer")
+public class BenutzerController {
+
+    @ModelAttribute("benutzer")
+    public BenutzerFormular initBenutzer(){
+        return new BenutzerFormular();
+    }
+
+    // @GetMapping("/benutzer")
+    // public String getBenutzer() {
+    //     return "benutzerbearbeiten";
+    // }
+
+    @PostMapping("/submit")
+    public String postBenutzerDaten(@ModelAttribute("benutzer") BenutzerFormular benutzerFormular, 
+                                    @RequestParam String name,
+                                    @RequestParam String eMail,
+                                    @RequestParam String passwort,
+                                    @RequestParam String geburtstag){
+        benutzerFormular.setName(name);
+        benutzerFormular.seteMail(eMail);
+        benutzerFormular.setPasswort(passwort);
+        benutzerFormular.setGeburtstag(geburtstag);
+        return "benutzerbearbeiten";
+    }
+
+    @GetMapping("/benutzer/{n}")
+    public String getBenutzerID(@PathVariable int n,
+                                Model model) {
+        model.addAttribute("benutzerID", n);
+        return "benutzerbearbeiten";
+    }
+    
+    
+}
---
+++ src/main/java/com/WebProjekt/MItfahrZentrale/ui/benutzer/BenutzerFormular.java
@@ -0,0 +1,36 @@
+package com.WebProjekt.MItfahrZentrale.ui.benutzer;
+
+public class BenutzerFormular {
+    String name;
+    String eMail;
+    String passwort;
+    String geburtstag;
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String geteMail() {
+        return eMail;
+    }
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+    public String getPasswort() {
+        return passwort;
+    }
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
+    public String getGeburtstag() {
+        return geburtstag;
+    }
+    public void setGeburtstag(String geburtstag) {
+        this.geburtstag = geburtstag;
+    }
+
+
+}
