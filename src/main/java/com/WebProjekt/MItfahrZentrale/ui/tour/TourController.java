43532438420d3ed7ba396fced0a9219f71c7b243
Verena <Verena.kipp@student.hs-rm.de>
2025-03-24 17:48:48 +0100

    FIX: vorausgefüllte Dropdowns

--- src/main/java/com/WebProjekt/MItfahrZentrale/ui/tour/TourController.java
+++ src/main/java/com/WebProjekt/MItfahrZentrale/ui/tour/TourController.java
@@ -16,9 +16,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
+import com.WebProjekt.MItfahrZentrale.entities.benutzer.Benutzer;
 import com.WebProjekt.MItfahrZentrale.entities.ort.Ort;
 import com.WebProjekt.MItfahrZentrale.entities.tour.Tour;
+import com.WebProjekt.MItfahrZentrale.services.benutzer.BenutzerServiceImpl;
 import com.WebProjekt.MItfahrZentrale.services.ort.OrtServiceImpl;
 import com.WebProjekt.MItfahrZentrale.services.tour.TourServiceImpl;
 
@@ -33,16 +36,31 @@ public class TourController {
 
     @Autowired TourServiceImpl tourService;
     @Autowired OrtServiceImpl ortService;
+    @Autowired BenutzerServiceImpl benutzerService;
 
     @ModelAttribute("tourformular")
     public TourFormular initTourFormular(){
         return new TourFormular();
     }
 
+    @GetMapping("/tour/{n}/del")
+    public String loescheTour(@PathVariable int n, Model model){
+
+        try{
+            tourService.loescheTourMitId(n);
+        }catch(Exception e){
+            model.addAttribute("info", e.getMessage());
+            logger.error(e.getMessage());
+        }
+
+        return "redirect:/tour";
+    }
+
     @GetMapping("/validiere")
     public String validiereFormular(@Valid @ModelAttribute("tourformular") TourFormular tourFormular,
                                     BindingResult result,
-                                    Model model){
+                                    Model model,
+                                    SessionStatus sessionStatus){
 
         if(result.hasErrors()){
             return "tourbearbeiten";
@@ -59,20 +77,24 @@ public class TourController {
                 model.addAttribute("info", e.getMessage());
             }
             
-        }else{ 
-            try{
-                tour = tourService.holeTourMitId(tour.getId()).orElseThrow(() -> new RuntimeException("Tour konnte nicht gefunden werden"));
-            }catch(Exception e){
-                model.addAttribute("info", e.getMessage());
-                logger.error("tour konnte in der Datenbannk nicht gefunden werden");
-            }
-        }
+         }//else{ 
+        //     try{
+        //         tour = tourService.holeTourMitId(tour.getId()).orElseThrow(() -> new RuntimeException("Tour konnte nicht gefunden werden"));
+        //     }catch(Exception e){
+        //         model.addAttribute("info", e.getMessage());
+        //         logger.error("Tour konnte in der Datenbannk nicht gefunden werden");
+        //     }
+        // }
 
         tourFormular.fromTour(tour);
         model.addAttribute("tourformular", tourFormular);
         model.addAttribute("tourID", tourFormular.getId());
         List<Ort> ortListe = ortService.holeAlleOrte();
+        List<Benutzer> anbieterListe = benutzerService.holeAlleBenutzer();
         model.addAttribute("ortListe", ortListe);
+        model.addAttribute("anbieterListe", anbieterListe);
+
+        sessionStatus.setComplete(); //session aktiv beenden
 
         return "tourbearbeiten";
 
@@ -85,18 +107,23 @@ public class TourController {
         
         Ort startOrt = ortService.holeOrtMitId(tourFormular.getStartOrtId()).orElseThrow(() -> new RuntimeException("Ort nicht gefunden"));
         Ort zielOrt = ortService.holeOrtMitId(tourFormular.getZielOrtId()).orElseThrow(() -> new RuntimeException("Ort nicht gefunden"));
+        Benutzer benutzer = benutzerService.holeBenutzerMitId(tourFormular.getAnbieterId()).orElseThrow(() -> new RuntimeException("Benutzer (Anbieter) nicht gefunden"));
 
         tourFormular.setStartOrt(startOrt);
         tourFormular.setZielOrt(zielOrt);
+        tourFormular.setAnbieter(benutzer);
 
         return "redirect:/validiere";
     }
 
     @GetMapping("/tour")
-    public String tourListeAusgeben(Model model){
+    public String tourListeAusgeben(SessionStatus sessionStatus,
+                                    Model model){
 
         List<Tour> alleTouren = tourService.holeAlleTouren();
         model.addAttribute("alleTouren", alleTouren);
+
+        sessionStatus.setComplete();
 
         return "tourliste";
     }
@@ -104,11 +131,13 @@ public class TourController {
     @GetMapping("/tour/0")
     public String tourAnlegen(@ModelAttribute("tourformular") TourFormular tourFormular,
                                 Model model){
-
+        
         List<Ort> ortListe = ortService.holeAlleOrte();
+        List<Benutzer> anbieterListe = benutzerService.holeAlleBenutzer();
         model.addAttribute("tourformular", tourFormular);
         model.addAttribute("tourID", 0);
         model.addAttribute("ortListe", ortListe);
+        model.addAttribute("anbieterListe", anbieterListe);
 
         return "tourbearbeiten";
     }
@@ -130,7 +159,9 @@ public class TourController {
         model.addAttribute("tourFormular", tourFormular);
         model.addAttribute("tourID", n);
         List<Ort> ortListe = ortService.holeAlleOrte();
+        List<Benutzer> anbieterListe = benutzerService.holeAlleBenutzer();
         model.addAttribute("ortListe", ortListe);
+        model.addAttribute("anbieterListe", anbieterListe);
 
         return "tourbearbeiten";
     }
--- src/main/java/com/WebProjekt/MItfahrZentrale/ui/tour/TourFormular.java
+++ src/main/java/com/WebProjekt/MItfahrZentrale/ui/tour/TourFormular.java
@@ -7,7 +7,6 @@ import com.WebProjekt.MItfahrZentrale.entities.ort.Ort;
 import com.WebProjekt.MItfahrZentrale.entities.tour.Tour;
 
 import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -42,6 +41,7 @@ public class TourFormular {
 
     private long startOrtId;
     private long zielOrtId; //Später wieder rausnehmen, falls es eine bessere Lösung gibt? 
+    private long anbieterId;
 
     public void toTour(Tour tour){
         tour.setId(this.id);
@@ -51,6 +51,7 @@ public class TourFormular {
         tour.setInfo(this.info);
         tour.setStartOrt(this.startOrt);
         tour.setZielOrt(this.zielOrt);
+        tour.setAnbieter(this.anbieter);
     }
 
     public void fromTour(Tour tour){
@@ -61,6 +62,7 @@ public class TourFormular {
         this.info = tour.getInfo();
         this.startOrt = tour.getStartOrt();
         this.zielOrt = tour.getZielOrt();
+        this.anbieter = tour.getAnbieter();
     }
 
     
@@ -153,6 +155,12 @@ public class TourFormular {
         this.zielOrt = zielOrt;
     }
 
-    
+    public long getAnbieterId() {
+        return anbieterId;
+    }
+
+    public void setAnbieterId(long anbieterId) {
+        this.anbieterId = anbieterId;
+    }
     
 }
